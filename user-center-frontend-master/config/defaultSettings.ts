7e96279a50e19c5753f6990cdc8700bb082ddf18
treeconnect <1494185219@qq.com>
2026-08-18 11:30:47 +0800

    更新 docker-compose 和数据库配置

--- user-center-frontend-master/config/defaultSettings.ts
+++ user-center-frontend-master/config/defaultSettings.ts
@@ -12,7 +12,7 @@ const Settings: LayoutSettings & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: '鱼皮用户中心',
+  title: '晨曦用户中心',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
