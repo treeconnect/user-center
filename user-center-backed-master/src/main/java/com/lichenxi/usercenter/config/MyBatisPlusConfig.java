cc29cbc69a9d79ebab1472edb6a67fa1b8db2242
Ubuntu <ubuntu@localhost.localdomain>
2026-08-20 20:24:57 +0800

    fix: 修复前端API代理配置、Nginx路径重复、Node版本升级及管理员账号设置

--- user-center-backed-master/src/main/java/com/lichenxi/usercenter/config/MyBatisPlusConfig.java
+++ user-center-backed-master/src/main/java/com/lichenxi/usercenter/config/MyBatisPlusConfig.java
@@ -3,7 +3,6 @@ package com.lichenxi.usercenter.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
  * @author https://github.com/liyupi
  */
 @Configuration
-@MapperScan("com.lichenxi.usercenter.mapper")
 public class MyBatisPlusConfig {
 
     /**
